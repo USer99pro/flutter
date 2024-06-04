feb33094a7f39e4f7117ff2f24166df4849642ed
USer99pro <akbbna48@gmail.com>
2024-06-04 12:11:17 +0700

    update 4/5/2567

--- lib/main.dart
+++ lib/main.dart
@@ -14,16 +14,73 @@ import 'package:flutter/material.dart';
 //   runApp(app);
 // }
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: "Phanumat",
-    home: Scaffold(
-        appBar: AppBar(
-           title: Text("Phanumat app 67"),
-           backgroundColor: Color.fromARGB(189, 48, 105, 58),
+// void main() {
+//   runApp(MaterialApp(
+//     debugShowCheckedModeBanner: false,
+//     title: "Phanumat",
+//     home: Scaffold(
+//         appBar: AppBar(
+//            title:const Text("Phanumat app 67"),
+//            backgroundColor: Color.fromARGB(189, 48, 105, 58),
+//       ),
+//     ),
+//    ),
+//   );
+// }
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp  extends StatelessWidget {
+  const MyApp ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: ' App 67' ,
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      home: CountArea(),
+    );
+  }
+}
+
+class CountArea extends StatefulWidget {
+  const CountArea({super.key});
+
+  @override
+  State<CountArea> createState() => _CountAreaState();
+}
+
+class _CountAreaState extends State<CountArea> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter'),
+
       ),
-    ),
-   ),
-  );
+
+      body:const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('กดปุ่มถ้าต้องการเพื่มจำนวนนับ',
+            style: TextStyle(
+              fontSize:25,
+              color: Colors.red,
+              letterSpacing: 0.75 //กำหนดความห่างระหว่างตัวอักษร
+
+            ),),
+            Text('0' ,
+            style: TextStyle(
+              fontSize: 48,
+              color: Colors.green),)
+          ],
+        ),
+      ),
+
+
+    );
+  }
 }
